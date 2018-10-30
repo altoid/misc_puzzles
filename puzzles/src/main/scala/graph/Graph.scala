d3d27a0aec6c751e15e40e983e1a02e7dc135067
Doug Tomm <dtomm@radiumone.com>
2018-10-30 08:56:40 -0700

    addNode/addNodes/addEdge and tests

--- puzzles/src/main/scala/graph/Graph.scala
+++ puzzles/src/main/scala/graph/Graph.scala
@@ -5,6 +5,10 @@ import scala.collection.mutable
 
 case class Node[A](data: A)
 
+object Node {
+  implicit def toNode[A](data: A) = Node(data)
+}
+
 // graph is a map of nodes to lists of nodes
 class Graph[A] extends mutable.HashMap[Node[A], scala.collection.mutable.Set[Node[A]]] {
   def addNode(n: Node[A]): Unit = {
@@ -15,4 +19,19 @@ class Graph[A] extends mutable.HashMap[Node[A], scala.collection.mutable.Set[Nod
       this += (n -> mutable.Set[Node[A]]())
     }
   }
+
+  def addNodes(n: Node[A]*): Unit = {
+    for (elem <- n) { this.addNode(elem) }
+  }
+
+  def addEdge(a: Node[A], b: Node[A]): Unit = {
+    if (!this.contains(a)) throw new IllegalStateException(s"node $a not in graph")
+    if (!this.contains(b)) throw new IllegalStateException(s"node $b not in graph")
+
+    this(a).add(b)
+  }
+}
+
+object Graph {
+  def apply[A](): Graph[A] = new Graph[A]()
 }
--- puzzles/src/test/scala/graph/GraphTest.scala
+++ puzzles/src/test/scala/graph/GraphTest.scala
@@ -4,5 +4,42 @@ import graph.{Graph,Node}
 import org.scalatest.FunSuite
 
 class GraphTest extends FunSuite {
+  test("node") {
+    val n = Node(3)
+    println(s"$n")
+    val m: Node[Int] = 4
+    println(s"$m")
+  }
 
+  test("graph - addNode") {
+    val g = Graph[Int]()
+    val n = Node(3)
+    g.addNode(n)
+    assertThrows[IllegalStateException] {
+      g.addNode(n)
+    }
+    g.addNode(4)
+  }
+
+  test("graph - addNodes") {
+    val g = Graph[Int]()
+    g.addNodes(1,2,3)
+    assert(g.contains(3))
+  }
+
+  test("graph - addEdge") {
+    val g = Graph[Int]()
+
+    assertThrows[IllegalStateException] {
+      g.addEdge(2, 3)
+    }
+    g.addNode(2)
+    assertThrows[IllegalStateException] {
+      g.addEdge(2, 3)
+    }
+    g.addNode(3)
+    g.addEdge(2, 3)
+
+    assert(g(2).contains(3))
+  }
 }
