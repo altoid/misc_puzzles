cb62c0d7086b1e2ed178e7598482dcb129616bce
Doug Tomm <dtomm@radiumone.com>
2018-11-01 04:23:20 -0700

    dfs implementation and test

--- puzzles/src/main/scala/graph/Graph.scala
+++ puzzles/src/main/scala/graph/Graph.scala
@@ -1,12 +1,16 @@
 package graph
 
 import scala.collection.mutable
-//import collection.mutable.{HashMap,Set}
+import scala.collection.mutable.ArrayBuffer
 
-case class Node[A](data: A)
+case class Node[A](data: A) extends Ordered[A] {
+  def compareTo(that: Node[A]): Int = this.data compare that.data
+}
 
 object Node {
   implicit def toNode[A](data: A) = Node(data)
+  implicit def intOrdering: Ordering[Node[Int]] = Ordering.by(n => n.data)
+  implicit def strOrdering: Ordering[Node[String]] = Ordering.by(n => n.data)
 }
 
 // graph is a map of nodes to lists of nodes
@@ -30,8 +34,55 @@ class Graph[A] extends mutable.HashMap[Node[A], scala.collection.mutable.Set[Nod
 
     this(a).add(b)
   }
+
+  def dfs(startHere: Node[A]): Array[A] = {
+    if (!this.contains(startHere)) throw new NoSuchElementException(s"node $startHere is not in the graph")
+
+    var result_buffer = ArrayBuffer[Node[A]]()
+
+    var visited = mutable.HashSet[Node[A]]()
+
+    var stack = mutable.Stack[Node[A]]()
+    stack.push(startHere)
+    result_buffer += startHere
+
+    def next_unvisited_neighbor(n: Node[A]): Option[Node[A]] = {
+      val ordering = implicitly[Ordering[Node[A]]]
+      var adj_list = this(n).toList.sortWith(ordering.lt(_, _))
+      adj_list.filterNot(x => visited.contains(x)).headOption
+    }
+
+    while (!stack.isEmpty) {
+      val top = stack.top
+      val k = next_unvisited_neighbor(top)
+
+      k match {
+        case Some(x) => {
+          stack.push(x)
+          visited.add(x)
+          result_buffer += x
+        }
+        case None => stack.pop()
+      }
+    }
+
+    var result = result_buffer.map(x => x.data).toArray
+    result
+  }
+}
+
+class UGraph[A] extends Graph[A] {
+  // undirected graph
+  override def addEdge(a: Node[A], b: Node[A]): Unit = {
+    super.addEdge(a, b)
+    super.addEdge(b, a)
+  }
 }
 
 object Graph {
   def apply[A](): Graph[A] = new Graph[A]()
+}
+
+object UGraph {
+  def apply[A](): UGraph[A] = new UGraph[A]()
 }
--- puzzles/src/test/scala/graph/GraphTest.scala
+++ puzzles/src/test/scala/graph/GraphTest.scala
@@ -1,14 +1,29 @@
 package graph
 
-import graph.{Graph,Node}
+import graph.{Graph, Node}
 import org.scalatest.FunSuite
 
 class GraphTest extends FunSuite {
   test("node") {
-    val n = Node(3)
-    println(s"$n")
+    val n = Node(4)
     val m: Node[Int] = 4
-    println(s"$m")
+    assert(n.data == m.data)
+  }
+
+  test("node cmp") {
+    val o_int = implicitly[Ordering[Node[Int]]]
+    val o_str = implicitly[Ordering[Node[String]]]
+
+    val a: Node[Int] = 3
+    val b: Node[Int] = 4
+    assert(o_int.lt(a, b))
+
+    val c: Node[String] = "three"
+    val d: Node[String] = "four"
+    val e: Node[String] = "four"
+    assert(o_str.lt(d, c))
+
+    assert(o_str.compare(d, e) == 0)
   }
 
   test("graph - addNode") {
@@ -23,7 +38,7 @@ class GraphTest extends FunSuite {
 
   test("graph - addNodes") {
     val g = Graph[Int]()
-    g.addNodes(1,2,3)
+    g.addNodes(1, 2, 3)
     assert(g.contains(3))
   }
 
@@ -41,5 +56,45 @@ class GraphTest extends FunSuite {
     g.addEdge(2, 3)
 
     assert(g(2).contains(3))
+  }
+
+  test("dfs") {
+    // cf. https://www.youtube.com/watch?v=zLZhSSXAwxI
+
+    val gr = UGraph[String]()
+
+    val a: Node[String] = "a"
+    val b: Node[String] = "b"
+    val c: Node[String] = "c"
+    val d: Node[String] = "d"
+    val e: Node[String] = "e"
+    val f: Node[String] = "f"
+    val g: Node[String] = "g"
+    val h: Node[String] = "h"
+
+    gr.addNodes(a, b, c, d, e, f, g, h)
+
+    gr.addEdge(a, b)
+    gr.addEdge(a, g)
+    gr.addEdge(a, d)
+
+    gr.addEdge(b, e)
+    gr.addEdge(b, f)
+
+    gr.addEdge(c, f)
+    gr.addEdge(c, h)
+
+    gr.addEdge(d, f)
+
+    gr.addEdge(e, g)
+
+    val outsider: Node[String] = "mr_lonely"
+
+    assertThrows[NoSuchElementException] {
+      gr.dfs(outsider)
+    }
+
+    var result = gr.dfs(a)
+    result foreach println
   }
 }
