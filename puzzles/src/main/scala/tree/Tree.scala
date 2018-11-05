c5f59e98ff3b3aa95682b40a63041d5bd6525895
Doug Tomm <dtomm@radiumone.com>
2018-11-05 07:29:48 -0800

    basic operations and tests

--- puzzles/src/main/scala/tree/Tree.scala
+++ puzzles/src/main/scala/tree/Tree.scala
@@ -1,17 +1,87 @@
 package tree
 
+import scala.annotation.tailrec
 import scala.math.Ordered._
 
-abstract class Tree[+A] {
+class Tree[A] {
+  class Node[A](val value: A, val nchildren: Int = 2) {
+    val children = Array.fill(nchildren)(None:Option[Node[A]])
+  }
+
+  object Node {
+    implicit def toNode[A](label: A) = Node[A](label)
+    def apply[A](label: A): Node[A] = new Node[A](label)
+  }
+
+  private var root: Option[Node[A]] = None
+
+  def addValue(v: A): Unit = {
+    // duplicate elements not allowed
+
+    @tailrec
+    def addValue_helper(node: Node[A], v: A): Unit = {
+      if (v == node.value) return
+      if (v.hashCode() < node.value.hashCode()) {
+        node.children(0) match {
+          case None => node.children(0) = Some(v)
+          case Some(n) => addValue_helper(n, v)
+        }
+      }
+      else {
+        node.children(1) match {
+          case None => node.children(1) = Some(v)
+          case Some(n) => addValue_helper(n, v)
+        }
+      }
+    }
+
+    root match {
+      case None => root = Some(v)
+      case Some(n) => addValue_helper(n, v)
+    }
+  }
+
+  def deleteValue(v: A): Unit = {
+    ???
+  }
+
+  def contains(v: A): Boolean = {
+    @tailrec
+    def contains_helper(node: Option[Node[A]], v: A): Boolean = {
+      node match {
+        case Some(n) => {
+          if (n.value == v) true
+          else if (v.hashCode() < n.value.hashCode()) {
+            contains_helper(n.children(0), v)
+          }
+          else contains_helper(n.children(1), v)
+        }
+        case None => false
+      }
+    }
+
+    contains_helper(root, v)
+  }
+
+  def height(): Int = {
+    def height_helper(node: Option[Node[A]]): Int = {
+      node match {
+        case None => 0
+        case Some(n) => 1 + math.max(height_helper(n.children(0)), height_helper(n.children(1)))
+      }
+    }
+
+    height_helper(root)
+  }
+
+  def size(): Int = {
+    def size_helper(node: Option[Node[A]]): Int = {
+      node match {
+        case None => 0
+        case Some(n) => 1 + size_helper(n.children(0)) + size_helper(n.children(1))
+      }
+    }
+
+    size_helper(root)
+  }
 }
-
-case class Node[+A](value: A, l: Tree[A], r: Tree[A]) extends Tree[A]
-case class Leaf[+A](value: A) extends Tree[A]
-case object Empty extends Tree[Nothing]
-
-
-
-//object Tree {
-//  implicit def toNode[A](label: A) = Tree[A](label, None:Option[Tree[A]], None:Option[Tree[A]])
-//  def apply[A](label: A, l: Option[Tree[A]], r: Option[Tree[A]]): Tree[A] = new Tree[A](label, l, r)
-//}
--- puzzles/src/test/scala/tree/TreeTest.scala
+++ puzzles/src/test/scala/tree/TreeTest.scala
@@ -4,7 +4,19 @@ import org.scalatest.FunSuite
 import roman.RomanNumeral
 
 class TreeTest extends FunSuite {
-  test("construction") {
-    val t: Tree[Int] = Node(1, Node(2, Empty, Leaf(9)), Node(3, Leaf(4), Leaf(5)))
+  test("basics") {
+    val t = new Tree[Int]()
+
+    assert(0 == t.size())
+    assert(0 == t.height())
+  }
+
+  test("singleton") {
+    val t = new Tree[Int]()
+
+    t.addValue(1)
+
+    assert(1 == t.size())
+    assert(1 == t.height())
   }
 }
