7fabe062bcc75f250a178326671e8e2726f031ef
Doug Tomm <dtomm@radiumone.com>
2019-04-10 08:22:43 -0700

    works

--- sql/median_of_groups/median2.sql
+++ sql/median_of_groups/median2.sql
@@ -2,7 +2,11 @@
 
 use hackerrank;
 
-select x.state, population, state_count, nn, state_count + 1 - nn rev
+select
+state, sum(population) / count(*) median
+from
+(
+select x.state, population, nn, state_count + 1 - nn rev
 from
 ( select @prev := '' collate utf8_swedish_ci, @n := 0) init
 join
@@ -13,7 +17,7 @@ select
 `state`,
 population
 from uscities
-where `state` in ('VI', 'RI')
+-- where `state` in ('VI', 'RI')
 order by `state`, population
 ) x
 inner join
@@ -23,6 +27,8 @@ from uscities
 group by state
 ) state_counts
 on state_counts.state = x.state
-
 where abs( (state_count + 1 - nn) - nn) <= 1
+) y
+group by state
+having median > 0
 ;
