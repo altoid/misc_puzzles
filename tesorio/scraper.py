f9d2443ae16b6b4cd9af2b60a13c3c789efea87a
Doug Tomm <dtomm@radiumone.com>
2019-04-19 13:07:29 -0700

    update scraper to use service instead of hitting mysql directly.

--- tesorio/scraper.py
+++ tesorio/scraper.py
@@ -9,39 +9,39 @@ import MySQLdb.cursors
 import argparse
 from contextlib import closing
 
-def update_repos():
-
-    # get all the repos we know about from the database.  delete any from the database that do not appear
-    # in the return result from the repo call.
-
-    repos = json.loads(repo_response.text)
-
-    select_repo = """
-select id
-from repo
-where owner_id = %(owner_id)s
-"""
-    select_args = {
-        'owner_id': 
-    insert_repo = """
-insert into repo
-(owner_id, id, url, json)
-values
-(%(owner_id)s, %(id)s, %(url)s, %(json)s)
-on duplicate key update
-json=values(json)
-"""
-
-    for repo in repos:
-        dml_args = {
-            'owner_id': repo['owner']['id'],
-            'id': repo['id'],
-            'url': repo['html_url'],
-            'json': repo_response.text
-            }
-        cursor.execute(insert_repo, dml_args)
-
-    print "%s repos" % len(repos)
+# def update_repos():
+# 
+#     # get all the repos we know about from the database.  delete any from the database that do not appear
+#     # in the return result from the repo call.
+# 
+#     repos = json.loads(repo_response.text)
+# 
+#     select_repo = """
+# select id
+# from repo
+# where owner_id = %(owner_id)s
+# """
+#     select_args = {
+#         'owner_id': 
+#     insert_repo = """
+# insert into repo
+# (owner_id, id, url, json)
+# values
+# (%(owner_id)s, %(id)s, %(url)s, %(json)s)
+# on duplicate key update
+# json=values(json)
+# """
+# 
+#     for repo in repos:
+#         dml_args = {
+#             'owner_id': repo['owner']['id'],
+#             'id': repo['id'],
+#             'url': repo['html_url'],
+#             'json': repo_response.text
+#             }
+#         cursor.execute(insert_repo, dml_args)
+# 
+#     print "%s repos" % len(repos)
 
 
 if __name__ == '__main__':
@@ -75,31 +75,15 @@ if __name__ == '__main__':
 
     repo_response = requests.get(repo_url, headers=headers)
     repos = json.loads(repo_response.text)
-#    print repo_response.status_code
+    # print repo_response.status_code
 
-    with closing(MySQLdb.connect(user=config.mysql_user,
-                                 passwd=config.mysql_passwd,
-                                 host=config.mysql_host,
-                                 db=config.mysql_db,
-                                 cursorclass=MySQLdb.cursors.DictCursor)) as conn:
-
-        cursor = conn.cursor()
-
-        insert_user = """
-insert into user
-(id, login, json)
-values (%(id)s, %(login)s, %(json)s)
-on duplicate key update
-json=values(json)
-"""
-        dml_args = {
+    dml_args = {
             'id': user_text_as_json['id'],
             'login': user_text_as_json['login'],
             'json': user_response.text
             }
 
-        cursor.execute(insert_user, dml_args)
+    put_response = requests.put("http://localhost:5000/user/%s" %user_text_as_json['login'],
+                                data = dml_args)
 
-        update_repos()
 
-        conn.commit()
---
+++ tesorio/service.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+
+from flask import Flask
+from flask_restful import Api, Resource, reqparse
+import config
+import MySQLdb
+import MySQLdb.cursors
+from contextlib import closing
+from pprint import pprint
+
+app = Flask(__name__)
+api = Api(app)
+
+class User(Resource):
+    def get(self, name):
+        return "hello", 200
+
+    def put(self, name):
+        parser = reqparse.RequestParser()
+        parser.add_argument("id")
+        parser.add_argument("login")
+        parser.add_argument("json")
+        args = parser.parse_args()
+
+        try:
+            with closing(MySQLdb.connect(user=config.mysql_user,
+                                         passwd=config.mysql_passwd,
+                                         host=config.mysql_host,
+                                         db=config.mysql_db,
+                                         cursorclass=MySQLdb.cursors.DictCursor)) as conn:
+     
+                cursor = conn.cursor()
+    
+                # check whether this login exists
+                select_user = """
+    select login
+    from user
+    where login = %s
+    """
+                cursor.execute(select_user, (args.login,))
+                rows = cursor.fetchone()
+                if rows:
+                    return_code = 200
+                else:
+                    return_code = 201
+    
+                insert_user = """
+    insert into user
+    (id, login, json)
+    values (%(id)s, %(login)s, %(json)s)
+    on duplicate key update
+    json=values(json)
+    """
+                dml_args = {
+                    'id': args.id,
+                    'login': args.login,
+                    'json': args.json,
+                    }
+    
+                cursor.execute(insert_user, dml_args)
+                conn.commit()
+    
+                return args.login, return_code
+        except Exception as e:
+            return args.login, 500
+
+api.add_resource(User, "/user/<string:name>")
+
+app.run(debug=True)
