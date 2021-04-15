2dc06cfeb095cdfd26bc00a33ddf23ecbe03e0bb
Doug Tomm <dctomm@gmail.com>
2021-04-14 22:07:42 -0700

    median populations by county

--- sql/median_of_groups/medians.sql
+++ sql/median_of_groups/medians.sql
@@ -1,9 +1,9 @@
 use hackerrank;
 
--- give me the median population by state
+-- give me the median population by state, among cities with > 0 population
 
 select
-    state, avg(population)
+state, avg(population)
 from
 (
     select
@@ -19,17 +19,65 @@ from
     	select state, population,
     	@rowid := if (state = @prev_state, @rowid + 1, 1) rowid,
     	@prev_state := state
-    	from uscities
+    	from
+	(
+		select state, population
+		from uscities
+		where population > 0
+	) nonzero_pop
     	order by state, population
     ) main
     inner join
     (
     	select state, count(*) cnt
     	from uscities
+	where population > 0
     	group by state
     ) statecount
     on statecount.state = main.state
 ) wrap1
-where selector <= 1
+where selector < 2
 group by state
+;
+
+-- median population by county
+
+select
+state, county_name, avg(population) county_median
+from
+(
+    select
+	main.state,
+	main.county_name,
+    	population,
+    	main.rowid upcount,
+    	cty_count.cnt - main.rowid + 1 downcount,
+       	abs(main.rowid - (cty_count.cnt - main.rowid + 1)) selector
+    from
+    (select @prev_state := null, @prev_county := null, @rowid := 0) init
+    inner join
+    (
+    	select state, county_name, population,
+    	@rowid := if (state = @prev_state and county_name = @prev_county, @rowid + 1, 1) rowid,
+    	@prev_state := state,
+    	@prev_county := county_name
+    	from
+    	(
+    		select state, county_name, population
+    		from uscities
+    		where population > 0
+    	) nonzero_pop
+    	order by state, county_name, population
+    ) main
+    inner join
+    (
+    	select state, county_name, count(*) cnt
+    	from uscities
+    	where population > 0
+    	group by state, county_name
+    ) cty_count
+    on cty_count.state = main.state and cty_count.county_name = main.county_name
+) wrap
+where selector < 2
+group by state, county_name
 ;
