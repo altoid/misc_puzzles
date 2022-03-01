da4116ac67fdcad49b285bc5cd5fd75007ffccb0
Doug Tomm <dtomm@Dougs-MBP.hsd1.ca.comcast.net>
2022-03-01 07:28:23 -0800

    read from file; don't use fileinput.

--- py/guidebook/guidebook.py
+++ py/guidebook/guidebook.py
@@ -1,25 +1,25 @@
 #!/usr/bin/env python3
 
 """
-read from a file and produce a string consisting of characters that appear exactly once in the file.
-characters are in the order they appear in the text.
+read from a file and produce a string consisting of characters
+that appear exactly once in the file.  characters are in the order
+they appear in the text.
 """
-
-import fileinput
 
 if __name__ == '__main__':
     char_to_count = {}
     char_to_first_location = {}
     char_counter = 0
 
-    for input_line in fileinput.input():
-        for c in input_line:
-            if c not in char_to_first_location:
-                char_to_first_location[c] = char_counter
-            if c not in char_to_count:
-                char_to_count[c] = 0
-            char_to_count[c] += 1
-            char_counter += 1
+    with open('quotation.txt') as f:
+        for input_line in f:
+            for c in input_line:
+                if c not in char_to_first_location:
+                    char_to_first_location[c] = char_counter
+                if c not in char_to_count:
+                    char_to_count[c] = 0
+                char_to_count[c] += 1
+                char_counter += 1
 
     solo_chars = [k for k, v in char_to_count.items() if v == 1]
 
