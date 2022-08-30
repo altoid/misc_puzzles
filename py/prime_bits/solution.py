893824bfc564cc9ce3f74c737c4886d1c3daf085
Doug Tomm <dougtomm@Dougs-MBP.hsd1.ca.comcast.net>
2022-08-30 08:55:08 -0700

    n_bigger_with_same_bits should include the given number in the count.

--- py/prime_bits/solution.py
+++ py/prime_bits/solution.py
@@ -9,8 +9,41 @@
 import unittest
 import random
 
-
 binary_coeff_cache = {}
+
+PRIMES = [
+    2, 3, 5, 7, 11, 13, 17, 19, 23, 29,
+    31, 37, 41, 43, 47, 53, 59, 61, 67, 71,
+    73, 79, 83, 89, 97, 101, 103, 107, 109, 113,
+    127, 131, 137, 139, 149, 151, 157, 163, 167, 173,
+    179, 181, 191, 193, 197, 199, 211, 223, 227, 229,
+    233, 239, 241, 251, 257, 263, 269, 271, 277, 281,
+    283, 293, 307, 311, 313, 317, 331, 337, 347, 349,
+    353, 359, 367, 373, 379, 383, 389, 397, 401, 409,
+    419, 421, 431, 433, 439, 443, 449, 457, 461, 463,
+    467, 479, 487, 491, 499, 503, 509, 521, 523, 541,
+    547, 557, 563, 569, 571, 577, 587, 593, 599, 601,
+    607, 613, 617, 619, 631, 641, 643, 647, 653, 659,
+    661, 673, 677, 683, 691, 701, 709, 719, 727, 733,
+    739, 743, 751, 757, 761, 769, 773, 787, 797, 809,
+    811, 821, 823, 827, 829, 839, 853, 857, 859, 863,
+    877, 881, 883, 887, 907, 911, 919, 929, 937, 941,
+    947, 953, 967, 971, 977, 983, 991, 997, 1009, 1013,
+    1019, 1021, 1031, 1033, 1039, 1049, 1051, 1061, 1063, 1069,
+    1087, 1091, 1093, 1097, 1103, 1109, 1117, 1123, 1129, 1151,
+    1153, 1163, 1171, 1181, 1187, 1193, 1201, 1213, 1217, 1223,
+    1229, 1231, 1237, 1249, 1259, 1277, 1279, 1283, 1289, 1291,
+    1297, 1301, 1303, 1307, 1319, 1321, 1327, 1361, 1367, 1373,
+    1381, 1399, 1409, 1423, 1427, 1429, 1433, 1439, 1447, 1451,
+    1453, 1459, 1471, 1481, 1483, 1487, 1489, 1493, 1499, 1511,
+    1523, 1531, 1543, 1549, 1553, 1559, 1567, 1571, 1579, 1583,
+    1597, 1601, 1607, 1609, 1613, 1619, 1621, 1627, 1637, 1657,
+    1663, 1667, 1669, 1693, 1697, 1699, 1709, 1721, 1723, 1733,
+    1741, 1747, 1753, 1759, 1777, 1783, 1787, 1789, 1801, 1811,
+    1823, 1831, 1847, 1861, 1867, 1871, 1873, 1877, 1879, 1889,
+    1901, 1907, 1913, 1931, 1933, 1949, 1951, 1973, 1979, 1987,
+    1993, 1997, 1999, 2003, 2011, 2017, 2027, 2029, 2039
+]
 
 
 def get_bits(n):
@@ -57,6 +90,7 @@ def decorate_zeroes(width, n):
         decoration += buffer
 
     return decoration
+
 
 # TODO: next we have to find all the numbers bigger than n with 1 fewer set bits than n.  then two fewer, etc.
 # for the 1 fewer case, find the first 0 following a 1.  set the 0 and clear the 1.  if there is a lower
@@ -113,7 +147,7 @@ def n_bigger_with_more_bits(n, width):
 
 def n_bigger_with_same_bits(n, width):
     """
-    given a number n with k bits set, find the number of numbers > n,
+    given a number n with k bits set, find the number of numbers >= n,
     expressible with <width> bits, that have exactly k bits set.
 
     consider:
@@ -154,7 +188,7 @@ def n_bigger_with_same_bits(n, width):
     """
 
     zeroes_decorated = decorate_zeroes(width, n)
-    total = 0
+    total = 1  # including n in the count
     for i in range(len(zeroes_decorated)):
         if zeroes_decorated[i] == 0:
             continue
@@ -224,40 +258,41 @@ class MyTest(unittest.TestCase):
     def test_n_bigger_more_bits_1(self):
         n = 358
         result = n_bigger_with_more_bits(n, 12)
-        self.assertEqual(2460, result)
+        self.assertEqual(2467, result)
 
     def test_n_bigger_more_bits_2(self):
         width = 12
         mask = 1
         allbits = 2 ** width - 1
         while mask < 2 ** width:
-            self.assertEqual(1, n_bigger_with_more_bits(allbits ^ mask, width))
+            self.assertEqual(2, n_bigger_with_more_bits(allbits ^ mask, width))
             mask <<= 1
 
     def test_n_bigger_same_bits_1(self):
         n = 358
         width = 12
-        expected = binary_coefficient(11, 4) + binary_coefficient(10, 4) + binary_coefficient(9, 4)
+        expected = 1
+        expected += binary_coefficient(11, 4) + binary_coefficient(10, 4) + binary_coefficient(9, 4)
         expected += binary_coefficient(7, 3) + binary_coefficient(4, 1) + binary_coefficient(3, 1)
         self.assertEqual(expected, n_bigger_with_same_bits(n, width))
 
     def test_n_bigger_same_bits_2(self):
-        self.assertEqual(0, n_bigger_with_same_bits(4095, 12))
+        self.assertEqual(1, n_bigger_with_same_bits(4095, 12))
 
     def test_n_bigger_same_bits_3(self):
         width = 12
-        self.assertEqual(0, n_bigger_with_same_bits(2 ** width - 1, width))
+        self.assertEqual(1, n_bigger_with_same_bits(2 ** width - 1, width))
 
     def test_n_bigger_same_bits_4(self):
-        self.assertEqual(3, n_bigger_with_same_bits(4087, 12))
+        self.assertEqual(4, n_bigger_with_same_bits(4087, 12))
 
     def test_n_bigger_same_bits_5(self):
-        self.assertEqual(0, n_bigger_with_same_bits(4094, 12))
+        self.assertEqual(1, n_bigger_with_same_bits(4094, 12))
 
     def test_n_bigger_same_bits_6(self):
         width = 12
         mask = 1
-        ngreater = 0
+        ngreater = 1
         allbits = 2 ** width - 1
         while mask < 2 ** width:
             self.assertEqual(ngreater, n_bigger_with_same_bits(allbits ^ mask, width))
