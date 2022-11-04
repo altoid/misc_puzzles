24df08f4ca0a09aa21c26988b6ced199f758d47e
Doug Tomm <dougtomm@Dougs-MBP.hsd1.ca.comcast.net>
2022-11-03 21:29:08 -0700

    maybe preprocessing step actually works

--- py/kmp.py
+++ py/kmp.py
@@ -4,63 +4,83 @@ import unittest
 from pprint import pprint
 
 
-# reference:  https://web.stanford.edu/class/cs97si/10-string-algorithms.pdf
+# references
+#
+# https://web.stanford.edu/class/cs97si/10-string-algorithms.pdf
+# http://www.cs.kent.edu/~dragan/DAAA/PatternMatching.pdf
 
 def preprocess(pattern):
     # returns pi table for pattern
     if not pattern:
         return []
 
-    length = len(pattern)
-    result = [0] * length
-
+    result = [0] * len(pattern)
     beginning = 0
-    counter = 0
     p = 1
-    while p < length:
-        if pattern[p] == pattern[beginning]:
-            counter += 1
-            result[p] = counter
-            beginning += 1
-        else:
-            counter = 0
+    prefixlen = 0
+    while p < len(pattern):
+        if pattern[p] != pattern[beginning]:
+            prefixlen = 0
+            result[p] = prefixlen
             beginning = 0
-        p += 1
+            if pattern[p] != pattern[beginning]:
+                p += 1
+        else:
+            prefixlen += 1
+            result[p] = prefixlen
+            beginning += 1
+            p += 1
 
-    result = [-1] + result
-    return result
-
+    return [-1] + result
 
 def match(text, pattern):
     # return the first index in text where pattern is found, or else None
     stop = len(text) - len(pattern) + 1
     if stop < 0:
-        return None
+        return -1
 
     pi_table = preprocess(pattern)
-    #print(pi_table)
+    print(pi_table)
 
     anchor = 0
     while anchor < stop:
-        # print("--------------")
-        # print(text)
-        # print("%s%s" % (' ' * anchor, pattern))
+        print("--------------")
+        print(text)
+        print("%s%s" % (' ' * anchor, pattern))
         p = 0
         while p < len(pattern) and pattern[p] == text[anchor + p]:
             p += 1
         if p == len(pattern):
             return anchor
 
-        # print("anchor is %s, mismatch at %s" % (anchor, anchor + p))
+        print("anchor is %s, p is %s, mismatch at %s" % (anchor, p, anchor + p))
         # p is the number of letters we've matched in this round.  if p is 0, then table[0] = -1, so
         # we will still shift by at least 1.
         shiftby = p - pi_table[p]
-        # print("shifting by %s" % shiftby)
+        print("shifting by %s" % shiftby)
         anchor += shiftby
 
+    return -1
+
+
+if __name__ == '__main__':
+    #pattern = 'aabaaac'
+    #pattern = 'abcdabcdabde'
+    #pattern = 'abcdabd'
+    pattern = 'aaacecaa'
+    result = preprocess(pattern)
+    print(result)
 
 class MatchTest(unittest.TestCase):
-    def test_1(self):
+    def test_3(self):
+        text = "aabaaabaaac"
+        pattern = "aabaaac"
+        expecting = 4
+
+        index = match(text, pattern)
+        self.assertEqual(expecting, index)
+
+    def test_2(self):
         text = 'ababababca'
         pattern = 'abc'
         expecting = 6
@@ -68,7 +88,7 @@ class MatchTest(unittest.TestCase):
         index = match(text, pattern)
         self.assertEqual(expecting, index)
 
-    def test_2(self):
+    def test_1(self):
         text = 'abc abcdab abcdabcdabde'
         pattern = 'abcdabd'
         expecting = 15
@@ -102,6 +122,13 @@ class PreprocessTest(unittest.TestCase):
     def test_4(self):
         pattern = 'abcdabd'
         expecting = [-1, 0, 0, 0, 0, 1, 2, 0]
+
+        pi_table = preprocess(pattern)
+        self.assertEqual(expecting, pi_table)
+
+    def test_5(self):
+        pattern = 'aabaaac'
+        expecting = [-1, 0, 1, 0, 1, 2, 1, 0]
 
         pi_table = preprocess(pattern)
         self.assertEqual(expecting, pi_table)
