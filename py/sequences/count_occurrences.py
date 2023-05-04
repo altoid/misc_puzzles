1af3311fb6ea22107feaae090737f51e46bb49a3
Doug Tomm <dougtomm@Dougs-MBP.hsd1.ca.comcast.net>
2023-05-03 23:31:59 -0700

    instrumentation

--- py/sequences/count_occurrences.py
+++ py/sequences/count_occurrences.py
@@ -5,8 +5,13 @@
 
 import unittest
 
+helper_calls = 0
 
-def helper(s, t, i, j):
+
+def helper(s, t, i, j, level):
+    print("%s(i = %s, j = %s)" % ("    " * level, i, j))
+    global helper_calls
+    helper_calls += 1
     if i == len(s) and j == len(t):
         return 1
 
@@ -15,16 +20,22 @@ def helper(s, t, i, j):
 
     count = 0
     if s[i] == t[j]:
-        count += helper(s, t, i + 1, j + 1)
-    count += helper(s, t, i + 1, j)
+        count += helper(s, t, i + 1, j + 1, level + 1)
+    count += helper(s, t, i + 1, j, level + 1)
 
     return count
+
 
 def count_subsequences(s, t):
     if len(t) > len(s):
         return 0
 
-    return helper(s, t, 0, 0)
+    global helper_calls
+    helper_calls = 0
+
+    result = helper(s, t, 0, 0, 0)
+    print("helper_calls:  %s" % helper_calls)
+    return result
 
 
 if __name__ == '__main__':
@@ -37,4 +48,3 @@ class MyTest(unittest.TestCase):
         t = 'abc'
         expecting = 3
         self.assertEqual(expecting, count_subsequences(s, t))
-
