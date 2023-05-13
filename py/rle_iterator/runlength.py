057cd60d7f0e412d234f897422a9e16497349e7f
Doug Tomm <dougtomm@Dougs-MBP.hsd1.ca.comcast.net>
2023-05-12 22:13:28 -0700

    use map to decode

--- py/rle_iterator/runlength.py
+++ py/rle_iterator/runlength.py
@@ -2,6 +2,7 @@
 
 from functools import reduce
 
+
 def enc_helper(acc, value):
     if acc[-1][0] == value:
         acc[-1][1] += 1
@@ -9,6 +10,7 @@ def enc_helper(acc, value):
         acc.append([value, 1])
 
     return acc
+
 
 def encode(s):
     if not s:
@@ -19,16 +21,17 @@ def encode(s):
 
     return result
 
+
 def decode(enc):
     return ''.join(list(map(lambda x: x[0] * x[1], enc)))
-    
+
+
 if __name__ == '__main__':
     encode_me = '88888!))*&&%%%%%%%%%%%'
     result = encode(encode_me)
 
     print(result)
 
-    result = decode_2(result)
+    result = decode(result)
 
     print(result)
-    
